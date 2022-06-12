445c596e549da7ca9a3030d9cee217b1fae43074
Varun Santhanam <talkto@vsanthanam.com>
2022-06-12 10:11:58 -0700

    Add DocC Plugin

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vsanthanam/NetworkReachability.git", from: "1.1.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0")
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
     ],
     targets: [
         .target(
